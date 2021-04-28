02fa0af9a560cd505d6308c594ef6142a5db65b9
randres <randr@DESKTOP-DVJNG83>
2021-04-27 23:33:56 -0600

    Crear Paciente

--- Proyecto/Controllers/HomeController.cs
+++ Proyecto/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Proyecto.Models;
+using System.Text.RegularExpressions;
 
 namespace Proyecto.Controllers
 {
@@ -17,14 +19,32 @@ namespace Proyecto.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        
+        public IActionResult Index()//Menu del Programa
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult CrearPaciente()//Creacion e ingreso de datos del usuario
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CrearPaciente(IFormCollection collection)
+        {
+            var NuevoPaciente = new Models.DatosPaciente { 
+            NombrePaciente=Regex.Replace(collection["NombrePaciente"], @"\s", "").ToUpper(),
+            ApellidoPaciente= Regex.Replace(collection["ApellidoPaciente"], @"\s", "").ToUpper(),
+            DPI = Regex.Replace(collection["DPI"], @"\s", ""),
+            PartidaDeNacimiento= collection["PartidaDeNacimiento"],
+            Departamento= Regex.Replace(collection["Departamento"], @"\s", "").ToUpper(),
+            Municipio = Regex.Replace(collection["Municipio"], @"\s", "").ToUpper()
+            };
+
+            return RedirectToAction("CrearCita");
+        }
+        public IActionResult CrearCita()
+        {
+
             return View();
         }
 
---
+++ Proyecto/Models/DatosPaciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto.Models
+{
+    public class DatosPaciente
+    {        
+        public string NombrePaciente { get; set; }
+        public string ApellidoPaciente { get; set; }
+        public string DPI { get; set; }
+        public string PartidaDeNacimiento { get; set; }
+        public string Departamento { get; set; }
+        public string Municipio { get; set; }
+
+
+    }
+}
