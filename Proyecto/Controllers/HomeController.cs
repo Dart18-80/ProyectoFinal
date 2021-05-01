c06fd27853c208edbb71f209bcd2ed723a012507
randres <randr@DESKTOP-DVJNG83>
2021-05-01 17:52:44 -0600

    sin error j

--- Proyecto/Controllers/HomeController.cs
+++ Proyecto/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Proyecto.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-]
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
